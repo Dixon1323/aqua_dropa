df6aacb0fe415ca898837959f52af6bd085fffb7
Dixon sunny <dixonsunny1323@gmail.com>
2024-08-06 15:50:46 +0530

    Add files via upload

---
+++ models.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    phone = db.Column(db.String(20), nullable=True)
+    address = db.Column(db.Text, nullable=True)
+    qr_code = db.Column(db.String(100), nullable=True)
+    role = db.Column(db.String(50), default='user')
+    password = db.Column(db.String(100))
+
+class Order(db.Model):
+    __tablename__ = 'orders'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user = db.relationship('User', backref=db.backref('orders', lazy=True))
+    quantity = db.Column(db.Integer, nullable=False)
+    product_type = db.Column(db.String(50), nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    status = db.Column(db.String(20), default='Pending')
